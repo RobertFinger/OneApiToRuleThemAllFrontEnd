8895e43a07cda861472c5e11e851138ef74ce88e
Bob Finger <robertfinger@yahoo.com>
2023-11-01 09:21:15 -0400

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'One-Api-To-Rule-Them-All-Front-End';
+}
